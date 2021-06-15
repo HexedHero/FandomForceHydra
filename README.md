c14983658ada97df2f0da232319d5fa43a9e275c
HexedHero <6012891+HexedHero@users.noreply.github.com>
2021-06-15 03:47:50 +0100

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# FandomForceHydra
+
+Force Fandom to use the Hydra theme.
+
+Dislike the new forced FandomDesktop? Install this and always use the old normal Hydra theme even when logged out!
+
+Install: TBA
---
+++ background.js
@@ -0,0 +1,40 @@
+(function ()
+{
+
+    chrome.webNavigation.onBeforeNavigate.addListener(
+
+        function (info)
+        {
+
+            const url = new URL(info.url);
+
+            // Check if fandom
+            if (!url.hostname.includes("fandom.com"))
+            {
+
+                return;
+
+            }
+
+            // Check if we are already using the Hydra theme
+            if (url.search.includes("useskin=hydra"))
+            {
+
+                return;
+
+            }
+
+            // Make new URL
+            const redirectUrl = url + "?useskin=hydra";
+
+            // Redirect
+            chrome.tabs.update(info.tabId,
+            {
+                url: redirectUrl
+            });
+
+        }
+
+    );
+
+})();

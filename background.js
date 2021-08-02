58101b7416c0b5c883d83b10f09f5c71c589c016
HexedHero <6012891+HexedHero@users.noreply.github.com>
2021-08-02 02:05:41 +0100

    Change to onCommitted
    Closes #1

--- background.js
+++ background.js
@@ -1,52 +1,59 @@
 (function ()
 {
 
-    chrome.webNavigation.onBeforeNavigate.addListener(
+    chrome.webNavigation.onCommitted.addListener(
 
         function (info)
         {
 
-            const url = new URL(info.url);
-
-            // Check if fandom
-            if (!url.hostname.includes("fandom.com"))
-            {
-
-                return;
-
-            }
-
-            var usingQuery = false;
-
-            // Check if we are already using the Hydra theme
-            if (url.search.includes("useskin=hydra"))
-            {
-
-                return;
-
-            }
-
-            // Check if we already have ? in the url
-            if (url.search.includes("?"))
-            {
-
-                usingQuery = true;
-
-            }
-
-            // Make new URL
-            const redirectUrl = url + (usingQuery ? "&" : "?") + "useskin=hydra";
-
-            // Redirect
-            chrome.tabs.update(info.tabId,
-            {
-
-                url: redirectUrl
-
-            });
+            processInfo(info)
 
         }
 
     );
 
 })();
+
+function processInfo(info)
+{
+
+    const url = new URL(info.url);
+
+    // Check if fandom
+    if (!url.hostname.includes("fandom.com"))
+    {
+
+        return;
+
+    }
+
+    var usingQuery = false;
+
+    // Check if we are already using the Hydra theme
+    if (url.search.includes("useskin=hydra"))
+    {
+
+        return;
+
+    }
+
+    // Check if we already have ? in the url
+    if (url.search.includes("?"))
+    {
+
+        usingQuery = true;
+
+    }
+
+    // Make new URL
+    const redirectUrl = url + (usingQuery ? "&" : "?") + "useskin=hydra";
+
+    // Redirect
+    chrome.tabs.update(info.tabId,
+    {
+
+        url: redirectUrl
+
+    });
+
+}
